508318ee086e342d90f3d04f335dd76f020a0e3f
Plebblan <62107332+Plebblan@users.noreply.github.com>
2025-04-15 10:17:29 +0700

    Add files via upload

--- assg_n/src/sys_killall.c
+++ assg_n/src/sys_killall.c
@@ -8,10 +8,12 @@
  * for the sole purpose of studying while attending the course CO2018.
  */
 
-#include "common.h"
-#include "syscall.h"
-#include "stdio.h"
-#include "libmem.h"
+ #include "common.h"
+ #include "syscall.h"
+ #include "stdio.h"
+ #include "libmem.h"
+ #include "string.h"
+ #include "queue.h"
 
 int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
 {
@@ -32,6 +34,29 @@ int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
         i++;
     }
     printf("The procname retrieved from memregionid %d is \"%s\"\n", memrg, proc_name);
+
+    #define TERMINATE_MATCHING(list) \
+        if (!empty(list)) { \
+            for (int j = 0; j < list->size; j++) { \
+                struct pcb_t *proc = list->proc[j]; \
+                char name_buf[100]; \
+                int k = 0; \
+                while(data != -1){ \
+                    libread(caller, memrg, i, &data); \
+                    name_buf[i]= data; \
+                    if(data == -1) name_buf[i]='\0'; \
+                    i++; \
+                } \
+                if (strcmp(name_buf, proc_name) == 0) { \
+                    printf("Terminating process: %s (pid=%d)\n", name_buf, proc->pid); \
+                    libfree(proc, proc->pid); \
+                    list->proc[j] = null; \
+                } \
+            } \
+        } \
+
+    TERMINATE_MATCHING(caller->running_list);
+    TERMINATE_MATCHING(caller->mlq_ready_queue);
 
     /* TODO: Traverse proclist to terminate the proc
      *       stcmp to check the process match proc_name
